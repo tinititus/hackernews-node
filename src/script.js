0ad6ba01fafbd29fd1a8ed1e0b5206bebe3c8f83
Tito <tito.carvalho@fdte.io>
2021-09-28 09:48:26 -0300

    add complete basic api

---
+++ src/script.js
@@ -0,0 +1,28 @@
+// 1
+const { PrismaClient } = require("@prisma/client")
+
+// 2
+const prisma = new PrismaClient()
+
+// 3
+async function main() {
+  // const newLink = await prisma.link.create({
+  //   data: {
+  //     description: "This is the first link",
+  //     url: "https://www.prisma.io",
+  //   }
+  // })
+
+  const allLinks = await prisma.link.findMany()
+  console.log(allLinks)
+}
+
+// 4
+main()
+  .catch(e => {
+    throw e
+  })
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
